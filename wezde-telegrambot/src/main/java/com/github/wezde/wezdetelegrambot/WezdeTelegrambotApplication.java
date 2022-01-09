11663fa027bc812e99d0e8a2a0804755f4d99529
wezde <90155906+wezde@users.noreply.github.com>
2022-01-09 19:34:44 +0300

    Add files via upload

---
+++ wezde-telegrambot/src/main/java/com/github/wezde/wezdetelegrambot/WezdeTelegrambotApplication.java
@@ -0,0 +1,13 @@
+package com.github.wezde.wezdetelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WezdeTelegrambotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WezdeTelegrambotApplication.class, args);
+	}
+
+}
---
+++ wezde-telegrambot/src/test/java/com/github/wezde/wezdetelegrambot/WezdeTelegrambotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wezde.wezdetelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WezdeTelegrambotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
