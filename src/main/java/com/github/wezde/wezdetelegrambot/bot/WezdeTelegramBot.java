993d3aed4aaf5c16e6d3d42ae1a94fa32e84343a
wezde <wezdewlez@yandex.ru>
2022-01-10 01:35:50 +0300

    TB-2: added stub telegram bot to the project.

---
+++ src/main/java/com/github/wezde/wezdetelegrambot/bot/WezdeTelegramBot.java
@@ -0,0 +1,46 @@
+package com.github.wezde.wezdetelegrambot.bot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class WezdeTelegramBot extends TelegramLongPollingBot {
+
+    @Value("${bot.username}")
+    private String username;
+
+    @Value("${bot.token}")
+    private String token;
+
+    @Override
+    public String getBotUsername() {
+        return username;
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText()) {
+            String message = update.getMessage().getText().trim();
+            String chatID = update.getMessage().getChatId().toString();
+
+            SendMessage sendMessage = new SendMessage();
+            sendMessage.setChatId(chatID);
+            sendMessage.setText(message);
+
+            try {
+                execute(sendMessage);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
